ef0a82d96628249b57f418d3e40fd0653028e93c
shenmadu <tashen.madu@gmail.com>
2024-03-15 21:00:47 +0530

    get all burrow book method add to controller

--- src/main/java/org/example/controller/BorrowBookController.java
+++ src/main/java/org/example/controller/BorrowBookController.java
@@ -1,18 +1,29 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.entity.BorrowBookEntity;
 import org.example.model.BorrowBook;
 import org.example.service.BorrowBookService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
 @RequestMapping("/borrow-Book")
+@Slf4j
 public class BorrowBookController {
     final BorrowBookService service;
     @PostMapping("/add")
     public void addBurrowBook(@RequestBody BorrowBook borrowBook){
+        log.info(borrowBook.toString());
         service.addBorrowBook(borrowBook);
+    }
+    @GetMapping("/get")
+    public List<BorrowBookEntity> getAll(){
+       return service.getBorrowBook();
+
     }
 }
